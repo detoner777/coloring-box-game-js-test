817672c35abed41b43c373f0c8cc473f1248ba85
detoner <detoner777@gmail.com>
2020-02-13 13:23:01 +0200

    starting basic stracture+API setup

---
+++ src/game-api/GameAPI.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+function getSassionDate() {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toString().split(" ")[1];
+  const year = date.getFullYear();
+  const time = date
+    .toString()
+    .split(" ")[4]
+    .slice(0, 5);
+
+  return `${day} ${month} ${year}, ${time} `;
+}
+
+export default class GameAPI {
+  constructor() {
+    this.url = "https://starnavi-frontend-test-task.herokuapp.com/";
+  }
+  getSettings() {
+    return axios
+      .get(`${this.url}game-settings`.then(res => res.data))
+      .catch(err => {
+        if (err.response) {
+          console.log(err.response.data);
+        }
+      });
+  }
+  getWinners() {
+    return axios.get(`${this.url}winners`.then(res => res.data)).catch(err => {
+      if (err.response) {
+        console.log(err.response.data);
+      }
+    });
+  }
+  postWinner({ winner, userName }) {
+    const data = {
+      winner: winner === "user" ? userName : "Computer",
+      date: getSassionDate()
+    };
+    return axios
+      .post(`winners`, data)
+      .then(res => res.data)
+      .catch(err => {
+        if (err.response) {
+          console.log(err.response.data);
+        }
+      });
+  }
+}
